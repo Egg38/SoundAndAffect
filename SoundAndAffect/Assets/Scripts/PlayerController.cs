8271e34cf2471f918f1f736d3d1544a0b577bfde
Egg38 <116301796+Egg38@users.noreply.github.com>
2023-03-24 10:54:38 -0700

    d

--- SoundAndAffect/Assets/Scripts/PlayerController.cs
+++ SoundAndAffect/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody playerRb;
+
     void Start()
     {
+        playerRb = GetComponent<Rigidbody>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+        }   
+    }  
+
 }
