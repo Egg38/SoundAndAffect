ca4bb0cbd74c356efa86e2686e3546628991eeb1
Egg38 <116301796+Egg38@users.noreply.github.com>
2023-03-24 11:40:34 -0700

    trying to figure out why spawn manager not working

--- SoundAndAffect/Assets/Scripts/PlayerController.cs
+++ SoundAndAffect/Assets/Scripts/PlayerController.cs
@@ -5,20 +5,26 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
-
+    private float jumpForce = 15;
+    public float gravityModifier;
+    public bool isOnGround = true;
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
-        
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
-            playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
         }   
-    }  
-
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
+    }
 }
